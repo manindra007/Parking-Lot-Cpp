45425a62ed383495e141fa01aa0ba4d63bbc9862
manindra007 <mns.manindra@gmail.com>
2021-10-09 01:44:38 +0530

    update structure

--- Vehicle/vehicle.cpp
+++ Vehicle/vehicle.cpp
@@ -8,30 +8,31 @@ class Vehicle{
     string registrationNumber;
     PersonDetail ownerName;
     VehicleType vehicleType;
-    public:
-        Vehicle(VehicleType vehicleType,string licencePlate,PersonDetail name){
-            setVehicleType(vehicleType);
-            setRegistrationNumber(licencePlate);
-            setOwnerName(name);
-        }
-        // setters
-        void setVehicleType(VehicleType vehicleType){
-            this->vehicleType=vehicleType;
-        }
-        void setRegistrationNumber(string regNumber){
-            this->registrationNumber=regNumber;
-        }
-        void setOwnerName(PersonDetail name){
-            this->ownerName=name;
-        }
-        //getters
-        VehicleType getVehicleType(){
-            return vehicleType;
-        }
-        string getLicencePlateNum(){
-            return registrationNumber;
-        }
-        PersonDetail getOnwerName(){
-            return ownerName;
-        }
+public:
+    Vehicle(VehicleType vehicleType,string licencePlate,PersonDetail name){
+        setVehicleType(vehicleType);
+        setRegistrationNumber(licencePlate);
+        setOwnerName(name);
+    }
+    // setters
+    void setVehicleType(VehicleType vehicleType){
+        this->vehicleType=vehicleType;
+    }
+    void setRegistrationNumber(string regNumber){
+        this->registrationNumber=regNumber;
+    }
+    void setOwnerName(PersonDetail name){
+        this->ownerName=name;
+    }
+    //getters
+    VehicleType getVehicleType(){
+        return vehicleType;
+    }
+    string getLicencePlateNum(){
+        return registrationNumber;
+    }
+    PersonDetail getOnwerName(){
+        return ownerName;
+    }
+    
 };
